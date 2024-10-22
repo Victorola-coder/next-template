c3667bb3ed5ae2868f08646beafa6f1cc2c400f0
Victorola <victoluolatunji@gmail.com>
2024-10-22 09:39:57 +0100

    ⚡️ [basic setup]

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,9 +8,76 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      fontFamily: {
+        // if you are using variable and want to use more than one fonts for your webapp,you could define it here. like i did 
+        geistSans: ["var(--font-geist-sans)"],
+      },
+      keyframes: {
+        "fade-effect": {
+          "0%": {
+            transform: "scale(0.9)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "scale(1)",
+            opacity: "1",
+          },
+        },
+
+        "slide-up": {
+          "0%": {
+            transform: "translateY(100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+        "slide-down": {
+          "0%": {
+            transform: "translateY(-100%)",
+            opacity: "0",
+          },
+          "100%": {
+            transform: "translateY(0)",
+            opacity: "1",
+          },
+        },
+        loader: {
+          "0%": {
+            opacity: "0.2",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
+        spin: {},
+      },
+      animation: {
+        "fade-in": "fade-effect 300ms linear",
+        "slide-down": "slide-down 300ms linear forwards",
+        "slide-up": "slide-up 300ms linear forwards",
+        "rotate-clockwise": "rotate-clockwise 1s infinite linear",
+        "loader-opacity": "loader 1s ease-in-out alternate infinite",
+        "spin-slow": "spin 20s linear infinite",
+      },
       colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+        primary: {
+          DEFAULT: "#8066FF",
+          100: "#6248E1",
+          200: "#8066FF",
+        },
+        dark: {
+          DEFAULT: "#141517",
+          100: "#414141",
+          200: "#121212",
+          300: "#676B71",
+        },
+        main: {
+          DEFAULT: "#E1DEF0",
+          100: "#F3EFFD",
+        },
       },
     },
   },
