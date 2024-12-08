fa4d146bd14ae2f67636928b8039f69e846da552
Victorola <victoluolatunji@gmail.com>
2024-12-08 16:07:56 +0100

    [components]++

--- app/components/global/index.ts
+++ app/components/global/index.ts
@@ -1,2 +1,6 @@
 export { default as AOS } from "./aos";
 export { default as AIO } from "./aio";
+export { default as Glow } from "./glow";
+export { default as Animation } from "./animation";
+
+// GLOBAL COMPONENTS export here
